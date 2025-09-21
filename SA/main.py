c3e8325512c1bee43186e4e00d387ace6ddfefbc
unknown <nahomkeneni4@gmail.com>
2025-09-21 20:57:32 +0300

    new

--- SA/main.py
+++ SA/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
+
+class model(BaseModel):
+    pass
+
+app.get("/")
+def read_root():
     return {"Hello": "World"}
 
+@app.get("/blogs?limit={linit}&published={published}")
+async def read_root(linit: int, published: bool = True):
+    return {"all blogs": "lost of all blogs"}
+
+
+app.get("/blogs/{id}")
+def show_blogs(id: int):
+    return {"blog_id": id}
+
+@app.post("/create_blogs")
+def create_blogs(request: model):
+    return {"data": "blog is beng created"}
