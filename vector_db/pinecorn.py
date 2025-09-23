812d66903f672cd2e7dcd9efe2b72a6e10805e70
unknown <nahomkeneni4@gmail.com>
2025-09-23 10:13:30 +0300

    chroma_db

--- vector_db/pinecorn.py
+++ vector_db/pinecorn.py
@@ -7,3 +7,15 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 textspliter = RecursiveCharacterTextSplitter(chunk_size=500, chunck_overlap=50)
 textspliter.split_text = lambda text: textspliter.split_text(text)
+
+persist_directory = 'db'
+
+embedding = OpenAIEmbeddings()
+
+vectordb = chroma.from_documents(documents="texts",
+                                 embedding= embedding,
+                                 persist_directory= persist_directory
+                                )
+
+vectordb.persist()
+vectordb = None
