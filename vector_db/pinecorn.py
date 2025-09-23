aa89aa3393cac924ab34db6b6f355608f2523ef5
unknown <nahomkeneni4@gmail.com>
2025-09-23 09:40:31 +0300

    text_spliter_added

--- vector_db/pinecorn.py
+++ vector_db/pinecorn.py
@@ -2,5 +2,6 @@ from langchain.vectorstores import chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import openai
 from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
