07e3d234bfd556b3bc2e2a2375afb9e140a79a4a
unknown <nahomkeneni4@gmail.com>
2025-09-23 10:52:55 +0300

    new

--- vector_db/chroma_db.py
+++ vector_db/chroma_db.py
@@ -3,7 +3,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import openai
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+from langchain.chains import RetrievalQA
 
 textspliter = RecursiveCharacterTextSplitter(chunk_size=500, chunck_overlap=50)
 textspliter.split_text = lambda text: textspliter.split_text(text)
@@ -19,3 +19,18 @@ vectordb = chroma.from_documents(documents="texts",
 
 vectordb.persist()
 vectordb = None
+
+chromadb = chroma(persist_directory=persist_directory,
+                  embedding_function=embedding)
+
+retriver = vectordb.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+retriver.get_relevant_documnets("What is the capital of France?")
+
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm = openai(),
+    chain_type = "stuff",
+    retriver = retriver,
+    return_source_documents=True
+)
---
+++ vector_db/pinecone.py
@@ -0,0 +1,22 @@
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import openai
+from langchain.vectorstores import pinecone
+from langchain.chains import retrieval_qa
+from langchain.prompts import PromptTemplate
+import os
+
+loader = PyPDFDirectoryLoader("pdfs")
+data = loader.Load()
+
+text_spliter = RecursiveCharacterTextSplitter(chunk = 500, chunck_overlap = 50)
+
+text_cuhunks = text_spliter.split_documents(data)
+
+os.environ['OPENAI_API_KEY'] = 'your_openai_api_key_here'
+os.environ['PINECONE_API_KEY'] = 'your_pinecone_api_key_here'
+
+embeddings = OpenAIEmbeddings()
+
+embeddings.embed_query = "hello"
